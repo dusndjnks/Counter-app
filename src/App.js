81eb227e987edb6c58fd5d3106c425e06bca7794
dusndjnks <mhdaffan0000@gmail.com>
2024-07-01 16:02:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+
+
+
+const[count,setCount]=useState(0)
+  const increase=()=>{
+  setCount(count<10?count+1:count)
+  console.log(count);
+}
+const decrease=()=>{
+  setCount(count>1?count-1:count)
+  console.log(count);
+}
+const reset=()=>{
+  setCount(0)
+  console.log(count);
+}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-300 h-screen  py-72 flex flex-col justify-center items-center gap-5">
+      <div>
+      <h1 className="text-2xl bg-black w-16 h-16 text-white flex items-center justify-center rounded-lg ">{count}</h1>
+      </div>
+      <div className="flex justify-center">
+      <div><button onClick={increase} className="border border-1 text-lg w-32 py-2 bg-gray-600 text-white">Increment</button></div>
+      <div><button onClick={decrease} className="border border-1 text-lg w-32 py-2 bg-gray-600 text-white">decrement</button></div>
+      <div><button onClick={reset} className="border border-1 text-lg w-32 py-2 bg-gray-600 text-white ">Reset</button></div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
